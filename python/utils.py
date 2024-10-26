831613723358dd8fd1f7e167372dfa0d7e3d04dc
GalaXai <92019780+GalaXai@users.noreply.github.com>
2024-10-26 23:39:52 +0200

    chore: different ruff config

--- python/utils.py
+++ python/utils.py
@@ -3,20 +3,20 @@ import os
 
 
 def load_data() -> dict:
-	data = {}
-	data_dir = "python/data"
+  data = {}
+  data_dir = "python/data"
 
-	for filename in os.listdir(data_dir):
-		if filename.endswith(".json"):
-			file_path = os.path.join(data_dir, filename)
-			with open(file_path, "r") as file:
-				file_data = json.load(file)
-				key = os.path.splitext(filename)[0]
-				data[key] = file_data["items"]
+  for filename in os.listdir(data_dir):
+    if filename.endswith(".json"):
+      file_path = os.path.join(data_dir, filename)
+      with open(file_path, "r") as file:
+        file_data = json.load(file)
+        key = os.path.splitext(filename)[0]
+        data[key] = file_data["items"]
 
-	return data
+  return data
 
 
 def get_full_day_name(day) -> str:
-	days = {"mon": "Monday", "tue": "Tuesday", "wed": "Wednesday", "thu": "Thursday", "fri": "Friday", "sat": "Saturday", "sun": "Sunday"}
-	return days.get(day[:3].lower(), day)
+  days = {"mon": "Monday", "tue": "Tuesday", "wed": "Wednesday", "thu": "Thursday", "fri": "Friday", "sat": "Saturday", "sun": "Sunday"}
+  return days.get(day[:3].lower(), day)
